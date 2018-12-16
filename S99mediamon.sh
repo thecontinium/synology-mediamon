d27e6cf0efce6c70329499fe2f6c31544c99365c
thecontinium <thecontinium@outlook.com>
2018-12-16 15:18:40 +0800

    added lang so that it would run under the synology task manager

--- S99mediamon.sh
+++ S99mediamon.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # /usr/syno/etc/rc.d/S99mediamon.sh
 
+export LANG='en_US.utf8'
+#env >&2
+
 case "$1" in
   start|"")
     #start the monitoring daemon
-    python3 /volume1/homes/admin/mediamon.py
+    python3 /volume1/homes/darthvader/mediamon.py
     ;;
   restart|reload|force-reload)
     echo "Error: argument '$1' not supported" >&2
